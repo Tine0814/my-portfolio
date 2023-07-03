c5d3d10572f952cb98c9282353e0ba841f899461
unknown <djhaybernardo@gmail.com>
2023-07-03 13:59:26 +0800

    update portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -79,6 +79,76 @@ const Portfolio = () => {
               className="mySwiper"
             >
               <SwiperSlide>
+                <div className="flex gap-10 flex-wrap">
+                  <div className="relative flex flex-col bg-white dark:bg-primaryDark gap-4 w-[369px] p-[30px] rounded-xl shadow-2xl">
+                    <div className="absolute top-2 left-3 flex gap-2">
+                      <div className="w-[10px] h-[10px] bg-[#ff605c] rounded-full"></div>
+                      <div className="w-[10px] h-[10px] bg-[#ffbd44] rounded-full"></div>
+                      <div className="w-[10px] h-[10px] bg-[#00ca4e] rounded-full"></div>
+                    </div>
+                    <div className="w-[294px] h-[220px] rounded-xl shadow-md flex justify-center items-center">
+                      <div className="w-full h-full overflow-hidden rounded-xl">
+                        <motion.img
+                          src={toDo}
+                          alt=""
+                          whileHover={{
+                            scale: 1.1,
+                            transition: { duration: 0.8 },
+                          }}
+                        />
+                      </div>
+                    </div>
+                    <h2 className="text-[30px]">ToDo LIST </h2>
+                    <div className="flex gap-2 justify-center">
+                      <IconButton
+                        textColor="text-white"
+                        text="MongoDB"
+                        color="bg-[#589636]"
+                      >
+                        <SiMongodb size={30} />
+                      </IconButton>
+                      <IconButton
+                        textColor="text-black"
+                        text="Express.js"
+                        color="bg-[white]"
+                      >
+                        <SiExpress size={30} />
+                      </IconButton>
+                      <IconButton
+                        textColor="text-white"
+                        text="React"
+                        color="bg-[#61DBFB]"
+                      >
+                        <FaReact size={30} />
+                      </IconButton>
+                      <IconButton
+                        textColor="text-white"
+                        text="Node.js"
+                        color="bg-[#68A063]"
+                      >
+                        <FaNodeJs size={30} />
+                      </IconButton>
+                      <IconButton
+                        textColor="text-white"
+                        text="Tailwind"
+                        color="bg-[#3490dc]"
+                      >
+                        <SiTailwindcss size={30} />
+                      </IconButton>
+                    </div>
+                    <div className="flex justify-center">
+                      <motion.a
+                        whileHover={{ scale: 1.2 }}
+                        href=""
+                        className="bg-primary text-light w-[90px] h-[30px] grid place-items-center rounded-md "
+                      >
+                        Visit Site{" "}
+                      </motion.a>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
                 {" "}
                 <div className="flex gap-10 flex-wrap">
                   <div className="relative flex flex-col bg-white dark:bg-primaryDark gap-4 w-[369px] p-[30px] rounded-xl shadow-2xl">
@@ -142,77 +212,6 @@ const Portfolio = () => {
                         whileHover={{ scale: 1.2 }}
                         href="https://clinicinventory.tresmariasresort.com/login"
                         target="_blank"
-                        className="bg-primary text-light w-[90px] h-[30px] grid place-items-center rounded-md "
-                      >
-                        Visit Site{" "}
-                      </motion.a>
-                    </div>
-                  </div>
-                </div>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <div className="flex gap-10 flex-wrap">
-                  <div className="relative flex flex-col bg-white dark:bg-primaryDark gap-4 w-[369px] p-[30px] rounded-xl shadow-2xl">
-                    <div className="absolute top-2 left-3 flex gap-2">
-                      <div className="w-[10px] h-[10px] bg-[#ff605c] rounded-full"></div>
-                      <div className="w-[10px] h-[10px] bg-[#ffbd44] rounded-full"></div>
-                      <div className="w-[10px] h-[10px] bg-[#00ca4e] rounded-full"></div>
-                    </div>
-                    <div className="w-[294px] h-[220px] rounded-xl shadow-md flex justify-center items-center">
-                      <div className="w-full h-full overflow-hidden rounded-xl">
-                        <motion.img
-                          src={toDo}
-                          alt=""
-                          whileHover={{
-                            scale: 1.1,
-                            transition: { duration: 0.8 },
-                          }}
-                        />
-                      </div>
-                    </div>
-                    <h2 className="text-[30px]">ToDo LIST </h2>
-                    <div className="flex gap-2 justify-center">
-                      <IconButton
-                        textColor="text-white"
-                        text="MongoDB"
-                        color="bg-[#589636]"
-                      >
-                        <SiMongodb size={30} />
-                      </IconButton>
-                      <IconButton
-                        textColor="text-black"
-                        text="Express.js"
-                        color="bg-[white]"
-                      >
-                        <SiExpress size={30} />
-                      </IconButton>
-                      <IconButton
-                        textColor="text-white"
-                        text="React"
-                        color="bg-[#61DBFB]"
-                      >
-                        <FaReact size={30} />
-                      </IconButton>
-                      <IconButton
-                        textColor="text-white"
-                        text="Node.js"
-                        color="bg-[#68A063]"
-                      >
-                        <FaNodeJs size={30} />
-                      </IconButton>
-                      <IconButton
-                        textColor="text-white"
-                        text="Tailwind"
-                        color="bg-[#3490dc]"
-                      >
-                        <SiTailwindcss size={30} />
-                      </IconButton>
-                    </div>
-                    <div className="flex justify-center">
-                      <motion.a
-                        whileHover={{ scale: 1.2 }}
-                        href=""
                         className="bg-primary text-light w-[90px] h-[30px] grid place-items-center rounded-md "
                       >
                         Visit Site{" "}
